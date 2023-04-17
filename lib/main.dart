bcd8e970f7ef06b1376c54eb58359d61260a9fa9
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-17 19:10:47 +0330

    change screen color

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz/screens/start_screen.dart';
+
 void main() {
-  runApp(const MyHomePage());
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder(child: Text('data'),);
-  }
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
