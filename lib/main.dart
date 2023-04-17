c9fa27540401faf4ccca4001180451e724ab4e49
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-04-17 18:06:57 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Text('data'),);
+  }
+}
